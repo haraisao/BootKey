d1bf1f629cfae809a2b0cbe06dd856b38f0e6120
Isao Hara <haraisao@gmail.com>
2021-09-27 09:38:20 +0900

    First commit

---
+++ BootKey/BootKey.ino
@@ -0,0 +1,40 @@
+/*
+ * Use HID-Project
+ */
+#include  "HID-Project.h"
+#include <EEPROM.h>
+
+#define ADDR  0x000
+void setup() {
+  ///// 
+  int csec = 40;
+  int i, n = 5;
+
+  csec = EEPROM.read(ADDR);
+
+  Keyboard.begin();
+
+  delay(csec*100);
+  for(i=0; i<n; ++i){
+    Keyboard.write(KEY_UP_ARROW);
+    delay(100);
+  }
+  Keyboard.write(KEY_ENTER);
+  Serial.begin(9600);
+}
+
+void loop() {
+  String buf;
+  int size = Serial.available();
+  if ( size > 1){
+    buf = Serial.readStringUntil("\n");
+    int csec = buf.toInt();
+    if(csec > 0){
+      EEPROM.write(ADDR, csec);
+      csec = EEPROM.read(ADDR);
+      Serial.print("Set delay：");
+      Serial.println(csec);
+    }
+  }
+  delay(3000);
+}
